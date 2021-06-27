fde98c8199a671aa09b56e318091b13d51532b4d
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-06-27 22:51:40 +0900

    Update UI.java

--- OopMiniPr_src_test/main/UI.java
+++ OopMiniPr_src_test/main/UI.java
@@ -14,28 +14,34 @@ public class UI {
 
 	// 전체 메뉴
 	public void start() {
-		System.out.println("1.회원 2.비회원 3.관리자 4.종료");
+
 		while (true) {
+			System.out.println("1.회원 2.비회원 3.관리자 4.종료");
 			int num = 0;
 
-			num = Integer.parseInt(getUserInput());
-			switch (num) {
-			// 1번은 회원의 기능
-			case 1:
-				menuMember();
-				break;
-			// 2번은 비회원 회원가입
-			case 2:
-				menuNoneMM();
-				break;
-			// 3번은 관리자 기능
-			case 3:
-				menuManager();
-				break;
-			// 4번은 db접속 해제후 프로그램 강제 종료
-			case 4:
-				mm.endDb();
-				System.exit(0);
+			try {
+				num = Integer.parseInt(getUserInput());
+
+				switch (num) {
+				// 1번은 회원의 기능
+				case 1:
+					menuMember();
+					break;
+				// 2번은 비회원 회원가입
+				case 2:
+					menuNoneMM();
+					break;
+				// 3번은 관리자 기능
+				case 3:
+					menuManager();
+					break;
+				// 4번은 db접속 해제후 프로그램 강제 종료
+				case 4:
+					mm.endDb();
+					System.exit(0);
+				}
+			} catch (NumberFormatException e) {
+				System.out.println("숫자만 입력하세요.");
 			}
 		}
 	}
@@ -52,35 +58,39 @@ public class UI {
 		// 로그인
 		if (mm.memberLogin() == true) {
 			while (true) {
+				try {
+					System.out.println("메뉴 선택을 해주세요.");
+					System.out.println("1.대여 2.반납 3.현재대여정보 4.전체차량정보 5.로그아웃");
 
-				System.out.println("메뉴 선택을 해주세요.");
-				System.out.println("1.대여 2.반납 3.현재대여정보 4.전체차량정보 5.로그아웃");
-				num = Integer.parseInt(getUserInput());
-				switch (num) {
+					num = Integer.parseInt(getUserInput());
+					switch (num) {
 
-				case 1:
-					// 대여 메서드
-					cm.rentCar2();
-					break;
-				case 2:
-					// 반납 메서드
-					cm.returnCar2();
-					break;
-				case 3:
-					// 현재 이용 정보
-					mm.currInfo();
-					break;
-				case 4:
-					// 차량 이용 현황 메서드
-					System.out.println("전체 차량 정보를 나타냅니다.");
-					cm.carList();
-					break;
-				case 5:
-					if (mm.Logout() == false) {
-						start();
-					} else {
+					case 1:
+						// 대여 메서드
+						cm.rentCar2();
 						break;
+					case 2:
+						// 반납 메서드
+						cm.returnCar2();
+						break;
+					case 3:
+						// 현재 이용 정보
+						mm.currInfo();
+						break;
+					case 4:
+						// 차량 이용 현황 메서드
+						System.out.println("전체 차량 정보를 나타냅니다.");
+						cm.carList();
+						break;
+					case 5:
+						if (mm.Logout() == false) {
+							start();
+						} else {
+							break;
+						}
 					}
+				} catch (NumberFormatException e) {
+					System.out.println("숫자만 입력하세요.");
 				}
 			}
 		} else {
@@ -101,47 +111,50 @@ public class UI {
 		System.out.println("관리자모드로 접속합니다.");
 
 		int num = 0;
-			if (mm.managerLogin() == true) {
+		if (mm.managerLogin() == true) {
 			while (true) {
-
-				System.out.println("메뉴를 선택해주세요.");
-				System.out.println("1.회원 리스트 2.회원 정보 삭제 3.차량 등록 4.차량 삭제 5.관리자 로그아웃");
-				num = Integer.parseInt(getUserInput());
-				switch (num) {
-				// 회원 리스트
-				case 1:
-					mm.memberList();
-					break;
-				// 회원 삭제
-				case 2:
-					mm.deletMember();
-					break;
-				// 차량 등록
-				case 3:
-					cm.addCar();
-					break;
-				// 차량 삭제
-				case 4:
-					cm.removeCar();
-					break;
-				// 관리자 로그아웃
-				case 5:
-					// 로그인 상태일 경우
-					if (mm.Logout() == false) {
-						start();
-					} else {
+				try {
+					System.out.println("메뉴를 선택해주세요.");
+					System.out.println("1.회원 리스트 2.회원 정보 삭제 3.차량 등록 4.차량 삭제 5.관리자 로그아웃");
+					num = Integer.parseInt(getUserInput());
+					switch (num) {
+					// 회원 리스트
+					case 1:
+						mm.memberList();
+						break;
+					// 회원 삭제
+					case 2:
+						mm.deletMember();
+						break;
+					// 차량 등록
+					case 3:
+						cm.addCar();
+						break;
+					// 차량 삭제
+					case 4:
+						cm.removeCar();
+						break;
+					// 관리자 로그아웃
+					case 5:
+						// 로그인 상태일 경우
+						if (mm.Logout() == false) {
+							start();
+						} else {
+							break;
+						}
+					default:
+						System.out.println("올바른 숫자를 입력하세요.");
 						break;
 					}
-				default:
-					System.out.println("올바른 숫자를 입력하세요.");
-					break;
+				} catch (NumberFormatException e) {
+					System.out.println("숫자만 입력하세요.");
 				}
 			}
 		} else {
 			menuManager();
 		}
 	}
-	
+
 	public static String getUserInput() {
 		Scanner scan = new Scanner(System.in);
 		String input = scan.nextLine();
