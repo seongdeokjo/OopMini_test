ebede97ea47fa31d519e2ebd2ddac7bae6814883
kohemf1 <84695772+kohemf1@users.noreply.github.com>
2021-06-28 13:32:22 +0900

    0628

--- OopMiniPr_src_test/manager/MemberManage.java
+++ OopMiniPr_src_test/manager/MemberManage.java
@@ -87,17 +87,17 @@ public class MemberManage {
 
 	// manager 로그인 메서드
 	public boolean managerLogin() {
-		boolean result = false;
+		boolean ck = false;
 		System.out.println("아이디를 입력하세요.");
 		String id = scan.nextLine();
 		System.out.println("비밀번호를 입력하세요.");
 		String pw = scan.nextLine();
 		if (dao.managerLogin(id, pw) > 0) {
-			result = true;
+			ck = true;
 		} else {
-			result = false;
+			ck = false;
 		}
-		return result;
+		return ck;
 	}
 
 	public boolean Logout() {
